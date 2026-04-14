323a4d2c36a669c3ff17ad1a259b4509e11574d9
Pekka Helesuo <pekka.helesuo@siili.com>
2026-04-14 13:34:49 +0300

    add og metadata for main page & blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,34 @@ import '../styles/main.scss'
 import type { Metadata } from 'next'
 import { leagueSpartan, mavenPro } from '@/utils/fonts'
 
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://oskari.org'
+const ogImage = '/assets/images/oskari_logo_black_horizontal.png'
+
 export const metadata: Metadata = {
+  metadataBase: new URL(siteUrl),
   title: {
     template: '%s - Oskari Map Application Platform',
     default: 'Oskari Map Application Platform',
   },
   description:
     'Oskari is a framework for easily building multipurpose web mapping applications utilizing distributed Spatial Data Infrastructures like INSPIRE.',
+  openGraph: {
+    type: 'website',
+    url: siteUrl,
+    siteName: 'Oskari Map Application Platform',
+    description:
+      'Oskari is a framework for easily building multipurpose web mapping applications utilizing distributed Spatial Data Infrastructures like INSPIRE.',
+    images: [
+      {
+        url: ogImage,
+        alt: 'Oskari Map Application Platform',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: [ogImage],
+  },
 }
 
 export default function RootLayout({
