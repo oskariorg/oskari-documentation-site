5cec2550ce059d509288439f7dbd373f4d971b2e
Pekka Helesuo <pekka.helesuo@siili.com>
2026-04-15 11:06:29 +0300

    add meta tags to docs as well

--- app/documentation/api/bundles/layout.tsx
+++ app/documentation/api/bundles/layout.tsx
@@ -1,6 +1,17 @@
 import DefaultLayout from '@/components/Layout'
+import type { Metadata } from 'next'
 import { Suspense } from 'react'
 import Loading from '../../docs/loading'
+
+export const metadata: Metadata = {
+  title: 'API Bundles',
+  openGraph: {
+    title: 'API Bundles',
+  },
+  twitter: {
+    title: 'API Bundles',
+  },
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/documentation/docs/layout.tsx
+++ app/documentation/docs/layout.tsx
@@ -1,6 +1,17 @@
 import DefaultLayout from '@/components/Layout'
+import type { Metadata } from 'next'
 import { Suspense } from 'react'
 import Loading from './loading'
+
+export const metadata: Metadata = {
+  title: 'Documentation',
+  openGraph: {
+    title: 'Documentation',
+  },
+  twitter: {
+    title: 'Documentation',
+  },
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
