93dda4a70a2b8b0525153b3a8ab45d876373a823
Sami Mäkinen <sami.makinen@nls.fi>
2026-09-22 13:05:38 +0300

    Generating dummy docs now also generates index for blogs

--- scripts/generateDummyVersions.js
+++ scripts/generateDummyVersions.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { execSync } = require('child_process');
 const { generateDocumentationMetadata } = require('./documentationMetadataHelper');
+
+// add index files for blogs etc
+execSync('node scripts/generateContentMetadata.js');
 
 // So build on GitHub Actions can work without cloning the documentation folders
 const generateDummyDocs = (folder, addIndex = true) => {
