ca767d03c20360f5e9a57351c2a1ed8b369a35a4
Sami Mäkinen <sami.makinen@nls.fi>
2026-09-22 13:07:59 +0300

    Remove unncesessary eslint configs

--- scripts/generateDummyVersions.js
+++ scripts/generateDummyVersions.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { execSync } = require('child_process');
 const { generateDocumentationMetadata } = require('./documentationMetadataHelper');
 
